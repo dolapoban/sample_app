bf26ed988f1d2618f65cbd9e47851aad5d83949b
dolapoban <dolapobanjoko@gmail.com>
2017-04-05 16:09:27 +0100

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,11 +7,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
   
-
-
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
+  test "should redirect index when not logged in" do
+    get users_path
     assert_redirected_to login_url
   end
 
@@ -21,4 +18,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              FILL_IN,
+                                            password_confirmation: FILL_IN,
+                                            admin: FILL_IN } }
+    assert_not @other_user.FILL_IN.admin?
+  end
+  
+
+  
 end
